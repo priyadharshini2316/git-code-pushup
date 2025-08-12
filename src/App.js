2285cccc6d20c08f84c0fef5465902e998ba13a9
Priyadharshini <pdarshini1223@gmail.com>
2025-08-12 15:23:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>PRIYA DHARSHINI</h1>
+        <h3>Mern Stack Developer</h3>
+        <p>Mern stack developer with 3+ years of experience building scalable web applications using React, Node. js, and MongoDB. ...
+specializing in front-end design and back-end architecture. Experienced with Angular, Express.</p>
+<button>More</button>
+
+      <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
